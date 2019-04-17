9274a4b4f3e7b184a7ab34d7076cdf0b699b3bc2
Cory Reed <swashcap@gmail.com>
2019-04-16 23:32:02 -0700

    feat: add logging

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,37 @@ import dotenvSafe from 'dotenv-safe'
 
 dotenvSafe.config()
 
+import good from 'good'
 import hapi from 'hapi'
 
 const init = async () => {
   const server = new hapi.Server({
-    port: process.env.PORT
+    port: process.env.PORT,
   })
+
+  if (process.env.NODE !== 'test') {
+    await server.register({
+      plugin: good,
+      options: {
+        ops: {
+          interval: 1000,
+        },
+        reporters: {
+          myConsoleReporter: [
+            {
+              module: 'good-squeeze',
+              name: 'Squeeze',
+              args: [{ log: '*', response: '*' }],
+            },
+            {
+              module: 'good-console',
+            },
+            'stdout',
+          ],
+        },
+      },
+    })
+  }
 
   await server.start()
 

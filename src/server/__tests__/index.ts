9404562b9625b647a563a547adeee294d71d015a
Cory Reed <swashcap@gmail.com>
2019-04-18 11:40:26 -0700

    test: add root route assertions

---
+++ src/server/__tests__/index.ts
@@ -0,0 +1,10 @@
+import { getServer } from '../index'
+
+test('server renders', async () => {
+  const server = await getServer()
+  const response = await server.inject('/')
+
+  expect(response.headers['content-type']).toMatch('text/html')
+  expect(response.statusCode).toBe(200)
+  expect(response.payload).toBeTruthy()
+})

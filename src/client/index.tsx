ce2e7402511d2cf20c11fd9eff14b73a3b319c90
Cory Reed <swashcap@gmail.com>
2019-04-21 22:52:47 -0700

    feat: add hot module handling to client entrypoint

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -5,13 +5,23 @@ import { render } from 'react-dom'
 import { App } from '../common/App'
 import { APP_ELEMENT_ID } from '../common/app-element-id'
 
-const el = document.getElementById(APP_ELEMENT_ID)
+const init = () => {
+  const el = document.getElementById(APP_ELEMENT_ID)
 
-if (el) {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    el,
-  )
+  if (el) {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      el,
+    )
+  }
 }
+
+// @ts-ignore
+if (module.hot) {
+  // @ts-ignore
+  module.hot.accept()
+}
+
+init()

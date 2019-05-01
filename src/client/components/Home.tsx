b1edfb328c2a1a62ede655d32863e761453da86f
Cory Reed <swashcap@gmail.com>
2019-04-30 23:58:50 -0700

    feat: add todos to home page

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Body1, Headline1 } from '@material/react-typography'
+import { Headline1 } from '@material/react-typography'
+import List from '@material/react-list'
 
 import { Page } from './Page'
+import { ProgressIndicator } from './ProgressIndicator'
 import { AppState } from '../reducers'
 import { AppDispatchProp } from '../store/configure-store'
 import { fetchTodos } from '../reducers/todos'
+import { ErrorMessage } from './ErrorMessage'
+import { Todo } from './Todo'
 
 export const Home = connect(state => state)(
   class extends React.Component<AppDispatchProp & AppState> {
@@ -14,12 +18,33 @@ export const Home = connect(state => state)(
     }
 
     render() {
-      return (
-        <Page>
-          <Headline1>Hello, world!</Headline1>
-          <Body1>This is the home page</Body1>
-        </Page>
-      )
+      const {
+        todos: { items, ui },
+      } = this.props
+      let content
+
+      if (!ui.fetched) {
+        content = <ProgressIndicator>Loading todos…</ProgressIndicator>
+      } else if (ui.fetched && ui.error) {
+        content = (
+          <ErrorMessage title="Failed to load todos">
+            {ui.error.message}
+          </ErrorMessage>
+        )
+      } else {
+        content = (
+          <div>
+            <Headline1>Todos</Headline1>
+            <List>
+              {Object.keys(items).map(id => (
+                <Todo key={id} todo={items[id]} />
+              ))}
+            </List>
+          </div>
+        )
+      }
+
+      return <Page>{content}</Page>
     }
   },
 )
